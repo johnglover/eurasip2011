f26ac9870f8c3f4fec78fc3b97cfde4e64b17ba9
John Glover <j@johnglover.net>
2013-04-16 09:37:13 +0200

    Always plot all results

--- plot_results.py
+++ plot_results.py
@@ -21,22 +21,13 @@ def plot_bars(file, results, labels, title, x_label, y_label):
     for bar in bars:
         height = bar.get_height()
         ax.text(bar.get_x() + bar.get_width() / 2., 1.05 * height,
-                '%.2f' % height, ha='center', va='bottom',
+                '%.2f' % (height + 0.005), ha='center', va='bottom',
                 fontsize=18)
     ax.set_ylabel(y_label, fontsize=18)
     ax.set_xlabel(x_label, fontsize=18)
     ax.set_xticks(indexes + 0.4)
     ax.set_xticklabels(labels, fontsize=14)
     plt.savefig(file, bbox_inches='tight')
-
-
-# plot results to files?
-plot_results = True
-
-# result types to calculate
-f_measure = True
-precision = True
-recall = True
 
 # ODFs to include in results
 odfs = ['EnergyODF', 'SpectralDifferenceODF', 'ComplexODF', 'LPEnergyODF',
@@ -50,42 +41,39 @@ odf_names = ['E', 'SD', 'CD', 'ELP', 'SDLP', 'CDLP', 'PAD']
 #              r'$\text{ODF}_{\rm PAD}$']
 
 num_onsets = modal.num_onsets()
-db = h5py.File('results.hdf5', 'r')
 rc('text', usetex=True)
 
-if plot_results:
-    if not os.path.exists('images'):
-        os.mkdir('images')
+if not os.path.exists('images'):
+    os.mkdir('images')
+
+db = h5py.File('results.hdf5', 'r')
 
 try:
-    if f_measure:
-        results = {}
-        for odf in odfs:
-            results[odf] = db['totals/odfs'][odf].attrs['f_measure']
+    # F-Measure
+    f_results = {}
+    for odf in odfs:
+        f_results[odf] = db['totals/odfs'][odf].attrs['f_measure']
 
-        if plot_results:
-            results = [results[odf] for odf in odfs]
-            plot_bars('images/avg_f_measure.png', results, odf_names,
-                      'Average F-Measure', 'Detection Functions', 'F-Measure')
+    f_results = [f_results[odf] for odf in odfs]
+    plot_bars('images/avg_f_measure.png', f_results, odf_names,
+              'Average F-Measure', 'Detection Functions', 'F-Measure')
 
-    if precision:
-        results = {}
-        for odf in odfs:
-            results[odf] = db['totals/odfs'][odf].attrs['precision']
+    # Precision
+    precision_results = {}
+    for odf in odfs:
+        precision_results[odf] = db['totals/odfs'][odf].attrs['precision']
 
-        if plot_results:
-            results = [results[odf] for odf in odfs]
-            plot_bars('images/avg_precision.png', results, odf_names,
-                      'Average Precision', 'Detection Functions', 'Precision')
+    precision_results = [precision_results[odf] for odf in odfs]
+    plot_bars('images/avg_precision.png', precision_results, odf_names,
+              'Average Precision', 'Detection Functions', 'Precision')
 
-    if recall:
-        results = {}
-        for odf in odfs:
-            results[odf] = db['totals/odfs'][odf].attrs['recall']
+    # Recall
+    recall_results = {}
+    for odf in odfs:
+        recall_results[odf] = db['totals/odfs'][odf].attrs['recall']
 
-        if plot_results:
-            results = [results[odf] for odf in odfs]
-            plot_bars('images/avg_recall.png', results, odf_names,
-                      'Average Recall', 'Detection Functions', 'Recall')
+    recall_results = [recall_results[odf] for odf in odfs]
+    plot_bars('images/avg_recall.png', recall_results, odf_names,
+              'Average Recall', 'Detection Functions', 'Recall')
 finally:
     db.close()
